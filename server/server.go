2230bcb1904a1e39cee0076bd8d275762e38e6cd
keijun.kumagai <keijun091221@gmail.com>
2020-12-19 14:45:47 +0900

    feat(photo): change field name

--- server/server.go
+++ server/server.go
@@ -15,10 +15,10 @@ type northPoleServer struct{}
 func (s *northPoleServer) JoinPublicMatch(userInfo *pb.UserInfo, stream pb.NorthPole_JoinPublicMatchServer) error {
 	for i := 0; i < 3; i++ {
 		testMatchInfo := &pb.MatchInfo{
-			Id:                  "c51a0ef2-550b-32e3-90e1-930f52691b0e",
-			Status:              pb.MatchStatus_Waiting,
-			CurrentNumberOfUser: 3,
-			MaxNumberOfUser:     4,
+			Id:                   "c51a0ef2-550b-32e3-90e1-930f52691b0e",
+			Status:               pb.MatchStatus_Waiting,
+			CurrentNumberOfUsers: 3,
+			MaxNumberOfUsers:     4,
 		}
 		stream.Send(testMatchInfo)
 	}
@@ -28,10 +28,10 @@ func (s *northPoleServer) JoinPublicMatch(userInfo *pb.UserInfo, stream pb.North
 func (s *northPoleServer) JoinPrivateMatch(userInfo *pb.UserInfo, stream pb.NorthPole_JoinPrivateMatchServer) error {
 	for i := 0; i < 3; i++ {
 		testMatchInfo := &pb.MatchInfo{
-			Id:                  "c0310d40-a450-3af9-9e4e-3f4e0f4c26df",
-			Status:              pb.MatchStatus_Waiting,
-			CurrentNumberOfUser: 3,
-			MaxNumberOfUser:     4,
+			Id:                   "c0310d40-a450-3af9-9e4e-3f4e0f4c26df",
+			Status:               pb.MatchStatus_Waiting,
+			CurrentNumberOfUsers: 3,
+			MaxNumberOfUsers:     4,
 		}
 		stream.Send(testMatchInfo)
 	}
@@ -40,10 +40,10 @@ func (s *northPoleServer) JoinPrivateMatch(userInfo *pb.UserInfo, stream pb.Nort
 
 func (s *northPoleServer) LeaveMatch(ctx context.Context, midAndUid *pb.MatchIDAndUserID) (*pb.MatchInfo, error) {
 	testMatchInfo := &pb.MatchInfo{
-		Id:                  "03240404-7d61-388f-8584-99a7e0438363",
-		Status:              pb.MatchStatus_Waiting,
-		CurrentNumberOfUser: 3,
-		MaxNumberOfUser:     4,
+		Id:                   "03240404-7d61-388f-8584-99a7e0438363",
+		Status:               pb.MatchStatus_Waiting,
+		CurrentNumberOfUsers: 3,
+		MaxNumberOfUsers:     4,
 	}
 	return testMatchInfo, nil
 }
